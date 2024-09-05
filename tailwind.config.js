c492e3fd2c81b11971b5d7b03d8d577c0169f3fb
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-09-05 22:51:00 +0300

    chore: changed color name

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        ourWhite: "#FDFDFF",
+        dark: "#393D3F",
+        darkGreen: "#62929E",
+        darkBlue: "#546A7B",
+        cream: "#C6C5B9",
+      },
       fontFamily: {
         arvo: ["Arvo", "serif"],
         roboto: ["Roboto", "Arial", "sans-serif"],
